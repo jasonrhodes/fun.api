8f3f9762b0d16b8e13f2f7ee0e34aa225675b577
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2012-05-30 15:46:52 -0400

    Initial commit after using composer to add Slim framework and adding a sample public/index.php file because I always forget to init earlier than this

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require "../vendor/autoload.php";
+
+$app = new Slim();
+
+/*
+$app->get("/", function () {
+    echo "You GOT it.";
+});
+
+$app->post("/", function () {
+    echo "You POSTED it.";
+});
+
+$app->put("/", function () {
+    header("Content-Type: application/json");
+    echo "{ 'message': 'put request sent' }";
+});
+
+$app->run();
+*/
